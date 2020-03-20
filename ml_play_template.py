0fdee723d5eeed05d99647a3fd64163ebe5bf4a2
sheng wen hsu <sha55616@gmail.com>
2020-03-20 23:43:22 +0800

    platform automatic

--- ml_play_template.py
+++ ml_play_template.py
@@ -22,7 +22,8 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
-
+    ball_x=100
+    ball_y=400
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
 
@@ -43,12 +44,36 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
-        # hiii
+        
+
+
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-
-        
+            dir_x=scene_info.ball[0]-ball_x
+            dir_y=scene_info.ball[1]-ball_y
+            ball_x=scene_info.ball[0]
+            ball_y=scene_info.ball[1]
+            if dir_y>0:
+                pla_x=(400-scene_info.ball[1])*(dir_x/dir_y)+scene_info.ball[0]
+                if pla_x>200:
+                    pla_x=200-(pla_x-200)
+                if pla_x<0:
+                    pla_x=0-pla_x    
+                if pla_x>scene_info.platform[0]+40:
+                    
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                elif pla_x<scene_info.platform[0]:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.NONE) 
+            else:
+                
+                if scene_info.ball[0]<scene_info.platform[0]:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                elif scene_info.ball[0]>scene_info.platform[0]:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT) 
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.NONE) 
